1f3ba1bde42436381e0368d014c877aac545b226
djscmd1125 <djscmd1125@gmail.com>
2020-10-04 00:23:19 +0900

    bot upload

--- index.js
+++ index.js
@@ -6,10 +6,45 @@ client.on('ready', () => {
   console.log('켰다.');
 });
 
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
 client.on('message', (message) => {
-  if(message.content === 'ping') {
-    message.reply('pong');
+  if(message.author.bot) return;
+
+  if(message.content == 'ping') {
+    return message.reply('pong');
+  }
+
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
   }
 });
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
 
 client.login(token);
